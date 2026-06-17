819e2f449173cd47750779bd9c97cfa17ecd7c68
ReygaDirga <reygadirg@gmail.com>
2026-06-17 14:17:01 +0700

    fix bug logout in profile page

--- app/src/main/java/com/example/dclassic/ProfileActivity.java
+++ app/src/main/java/com/example/dclassic/ProfileActivity.java
@@ -3,13 +3,15 @@ package com.example.dclassic; // Pastikan package ini sesuai dengan punyamu
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
     ImageView btnBack;
+    ImageView btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +36,32 @@ public class ProfileActivity extends AppCompatActivity {
 
 
         txtUsername.setText(username);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(v -> {
+            SharedPreferences prefss =
+                    getSharedPreferences(
+                            "USER_SESSION",
+                            MODE_PRIVATE
+                    );
+
+            prefss.edit()
+                    .clear()
+                    .apply();
+
+            Intent intent =
+                    new Intent(
+                            ProfileActivity.this,
+                            LoginActivity.class
+                    );
+
+            intent.setFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK
+            );
+
+            startActivity(intent);
+            finish();
+        });
     }
 }
